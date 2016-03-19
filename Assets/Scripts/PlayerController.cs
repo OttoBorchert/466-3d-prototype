4546e869f2d22f5558a3b79a7917bd61a25da8d8
Otto Borchert <OttoBorchert@gmail.com>
2016-03-18 17:13:41 -0700

    Added basic multiplayer movement to our game. I took out the NetworkBall and NetworkThrow scripts because we'll be going over those on Monday.

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,101 @@
+using UnityEngine;
+
+[RequireComponent(typeof(PlayerMotor))]
+public class PlayerController : MonoBehaviour {
+
+	[SerializeField]
+	private float speed = 5f;
+	[SerializeField]
+	private float lookSensitivity = 3f;
+
+	[SerializeField]
+	private float thrusterForce = 1000f;
+
+	//Making sure the cursor is locked, like in a normal FPS
+	public bool lockCursor = true;
+	private bool m_cursorIsLocked = true;
+
+	private PlayerMotor motor;
+
+
+	void Start ()
+	{
+		motor = GetComponent<PlayerMotor>();
+	}
+
+	void Update ()
+	{
+		//Calculate movement velocity as a 3D vector
+		float _xMov = Input.GetAxisRaw("Horizontal");
+		float _zMov = Input.GetAxisRaw("Vertical");
+
+		Vector3 _movHorizontal = transform.right * _xMov;
+		Vector3 _movVertical = transform.forward * _zMov;
+
+		// Final movement vector
+		Vector3 _velocity = (_movHorizontal + _movVertical).normalized * speed;
+
+		//Apply movement
+		motor.Move(_velocity);
+
+		//Calculate rotation as a 3D vector (turning around)
+		float _yRot = Input.GetAxisRaw("Mouse X");
+
+		Vector3 _rotation = new Vector3(0f, _yRot, 0f) * lookSensitivity;
+
+		//Apply rotation
+		motor.Rotate(_rotation);
+
+		//Calculate camera rotation as a 3D vector (turning around)
+		float _xRot = Input.GetAxisRaw("Mouse Y");
+
+		float _cameraRotationX = _xRot * lookSensitivity;
+
+		//Apply camera rotation
+		motor.RotateCamera(_cameraRotationX);
+
+		// Calculate the thrusterforce based on player input
+		Vector3 _thrusterForce = Vector3.zero;
+		if (Input.GetButton ("Jump"))
+		{
+			_thrusterForce = Vector3.up * thrusterForce;
+		}
+
+		// Apply the thruster force
+		motor.ApplyThruster(_thrusterForce);
+
+		UpdateCursorLock ();
+	}
+
+	public void UpdateCursorLock()
+	{
+		//if the user set "lockCursor" we check & properly lock the cursos
+		if (lockCursor)
+			InternalLockUpdate();
+	}
+
+
+	private void InternalLockUpdate()
+	{
+		if(Input.GetKeyUp(KeyCode.Escape))
+		{
+			m_cursorIsLocked = false;
+		}
+		else if(Input.GetMouseButtonUp(0))
+		{
+			m_cursorIsLocked = true;
+		}
+
+		if (m_cursorIsLocked)
+		{
+			Cursor.lockState = CursorLockMode.Locked;
+			Cursor.visible = false;
+		}
+		else if (!m_cursorIsLocked)
+		{
+			Cursor.lockState = CursorLockMode.None;
+			Cursor.visible = true;
+		}
+	}
+
+}
